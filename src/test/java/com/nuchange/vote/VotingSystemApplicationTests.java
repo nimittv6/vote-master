3ef24d6cefae19c3fe3d62829bed7d162a2660f1
nimitt <nimittv6@gmail.com>
2023-12-14 01:35:05 +0530

    added application property, architecture and test cases

---
+++ src/test/java/com/nuchange/vote/VotingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuchange.vote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
